8461f4cb495f69d9149a93ff37037314b04a84c2
SK-Ren <SK-Ren@users.noreply.github.com>
2015-07-08 00:57:11 -0700

    Capstone Project
    Root for SDV Capstone Project at ITT Tech, Seattle 07/08/2015

---
+++ CapstoneProject/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+	
+	protected ActorController2D actorControl; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		actorControl = gameObject.GetComponent<ActorController2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+			actorControl.Jump();
+
+		// Assigned Attack/Action Button
+		if (Input.GetKeyDown (KeyCode.F))
+			actorControl.Attack();
+
+		// Actor class Horizontal Input
+		actorControl.horizontalAxis = Input.GetAxis ("Horizontal");
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+
+}
